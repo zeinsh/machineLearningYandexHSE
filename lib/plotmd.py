18d23d2e4a07a8b7331e055d91e000f2f3a3e590
zeinsh <zeineshahine@gmail.com>
2017-11-05 02:04:52 +0300

    plot 2D features with decision boundary

--- lib/plotmd.py
+++ lib/plotmd.py
@@ -4,13 +4,19 @@
 # In[ ]:
 
 
-import matplotlib.pyplot as plt
-import matplotlib
 
 
 # In[2]:
 
 def scatterData(X,y,classmap):
+    import matplotlib.pyplot as plt
     for c,label in classmap.iteritems():
         pltdata=X[(y==c)]
         plt.scatter(pltdata[:,0], pltdata[:,1] ,label=label) 
+
+def plotDecisionBoundary(w,bias,fig_boundary):
+    import matplotlib.pyplot as plt
+    from helpersmd import getLine
+    xline,yline=getLine(w,bias,fig_boundary)
+    plt.plot(xline,yline, 'k-',label="Decision Boundary")
+
